facb0996d0fdd4d5e34751f0f26119b20f36b729
Matias <mbarreto@provinciamicrocreditos.com>
2024-09-19 15:39:11 -0300

    update rutas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,11 @@ function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clea
                 <div className="container-xl">
                     <div className="row justify-content-center justify-content-md-between">
                         <div className="col-8 col-md-3">
-                            <a href="index.html"> <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo"/> </a>
+                            <a href="index.html"> <img className="img-fluid" src="/img/logo.svg" alt="imagen logo"/> </a>
                         </div>
                         <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                             <div className="carrito">
-                                <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito"/>
+                                <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito"/>
                                 <div id="carrito" className="bg-white p-3">
                                     {
                                         isEmply ? ( <p className="text-center">El carrito esta vacio</p> ):(
@@ -34,7 +34,7 @@ function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clea
                                                         { 
                                                             cart.map( guitar =>(                                                
                                                                 <tr key={guitar.id}>
-                                                                    <td> <img className="img-fluid" src={`./public/img/${guitar.image}.jpg`} alt="imagen guitarra"/> </td>
+                                                                    <td> <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt="imagen guitarra"/> </td>
                                                                     <td> {guitar.name} </td>
                                                                     <td className="fw-bold"> ${guitar.price} </td>
                                                                     <td className="flex align-items-start gap-4"> 
